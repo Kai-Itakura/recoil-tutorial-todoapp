28672e060ac2f655733f0bda351391461233ae91
Kai-Itakura <itakai199969@gmail.com>
2023-07-09 01:08:35 +0900

    学習完了

---
+++ src/components/AddTask.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './AddTask.css'
+import { addTitleState, addTitleStateLength } from '../states/addTitleState'
+import { useRecoilValue } from 'recoil'
+
+const AddTask = () => {
+  const addTitle = useRecoilValue(addTitleState)
+  const addTitleLength = useRecoilValue(addTitleStateLength)
+
+  return (
+    <div className='taskField'>
+      <div>{addTitleLength}個のタスクがあります。</div>
+      <ul>
+        {addTitle.map((task) => (
+          <li key={task.id}>{task.title}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default AddTask
